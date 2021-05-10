fd262d8f3d5f4f050204b6e4947b022bee5f98f3
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-05-11 00:29:30 +0200

    multiply btn fix

--- script.js
+++ script.js
@@ -8,26 +8,39 @@ cookieClick.addEventListener("click",function(){
     document.getElementById("displayCount").innerHTML = countCookie;
 });
 
+// //sound effect
+// let bleep = new Audio();
+// bleep.src = "click_sound.wav";
+// function playBtnSound(num){
+//     bleep.play();
+// };
+
+
 //reset button
 let resetBtn = document.getElementById("reset");
 
 resetBtn.addEventListener("click", function(){
     countCookie = 0;
     document.getElementById("displayCount").innerHTML = countCookie;
-})
+});
+
+let multiplyBtn = document.getElementById("multiply");  
 
 
-if(countCookie > 10){
-  //when it's under 100, make the button disabled
-  let multiplyBtn = document.getElementById("multiply");
-  multiplyBtn.classList.toggle("disabled");
+//multiply button function
+multiplyBtn.addEventListener("click", function(){
+    //when it's over 20, make the button clickable
+    if(countCookie >= 20){
+        if(countCookie == 20){
+            countCookie = countCookie * 2;
+            document.getElementById("displayCount").innerHTML = countCookie;
+        }else if(countCookie >= 40){
+            countCookie = countCookie * 2 -20;
+            document.getElementById("displayCount").innerHTML = countCookie;
+        }
+        
+    };
+});
 
-    // function multiplyBtn(){
-    //     //multiply button function
-    //     multiplyBtn.addEventListener("click", function(){
-    //         countCookie *= 2;
-    //         document.getElementById("displayCount").innerHTML = countCookie;
-    //     });
-    // }
-};
+
 
