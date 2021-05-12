f1763ef6b8529e232718097bdff541258666094b
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-05-12 11:26:39 +0200

    bonus btn fixed

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let cookieClick = document.getElementById("cookie");
 let countCookie = 0;
 let clickPoint = 0;
 document.getElementById("cookieCount").innerHTML = countCookie;
-document.getElementById("pointCount").innerHTML = clickPoint;
+document.getElementById("clickCount").innerHTML = clickPoint;
 
 //click and count cookie
 cookieClick.addEventListener("click",function(){
     countCookie += 1;
     document.getElementById("cookieCount").innerHTML = countCookie;
-    //count point
+    //count click
     clickPoint += 1;
-    document.getElementById("pointCount").innerHTML = clickPoint;
+    document.getElementById("clickCount").innerHTML = clickPoint;
 });
 
 
@@ -25,6 +25,7 @@ let cost = multiNumber*10;
         
         multiNumber+=1;
         countCookie = countCookie - cost;
+        document.getElementById("cookieCount").innerHTML = countCookie;
 
         document.getElementById("multiNum").innerHTML = multiNumber;
         document.getElementById("multiNum2").innerHTML = multiNumber;
@@ -45,6 +46,8 @@ autoBtn.addEventListener("click",function(){
     if(countCookie >=100){
 
         countCookie = countCookie - 100; //purchase
+        document.getElementById("cookieCount").innerHTML = countCookie;
+        
         setInterval(addCookie,1000);
 
         function addCookie(){
@@ -52,5 +55,41 @@ autoBtn.addEventListener("click",function(){
             document.getElementById("cookieCount").innerHTML = countCookie;
         }
         autoBtn.disabled = true;
+    }
+});
+
+//bonus button (for 30sec 200%)
+let bonusBtn = document.getElementById("bonus"); 
+
+bonusBtn.addEventListener("click",function(){
+    if(countCookie >=100){
+        countCookie = countCookie - 100; //purchase
+        document.getElementById("cookieCount").innerHTML = countCookie;
+
+        //timer
+        let timeLeft = 30;
+        let showTimer = document.getElementById("timer");
+        let timer = setInterval(countdown, 1000);//set the countdown to every second
+
+        function countdown(){
+            if (timeLeft == -1){
+                clearTimeout(timer);
+                showTimer.innerHTML = "No more Bonus!";
+            }else{
+                showTimer.innerHTML = "Bonus time" + timeLeft;
+                timeLeft--; //we subtract the second each iteration
+            };
+        };
+        
+        cookieClick.addEventListener("click",function(){
+            if(timeLeft >= 0){
+                countCookie *=2;
+                document.getElementById("cookieCount").innerHTML = countCookie;
+            }else if(timeLeft == -1){
+                countCookie *=1;
+                document.getElementById("cookieCount").innerHTML = countCookie;
+            };
+        });
+        bonusBtn.disabled = true;
     }
 });
